735c346f85396eef6ee74f02250a76ea0ea9f245
Arturo Perez <VrturoPerez@gmail.com>
2017-10-21 20:46:32 -0700

    add external class

--- externalSort/src/main/java/com/sorting/App.java
+++ externalSort/src/main/java/com/sorting/App.java
@@ -9,20 +9,13 @@ import java.io.IOException;
  *
  */
 public class App {
-    public static void main( String args[]) throws IOException {
-        System.out.println( "Hello World!" );
-    }
+    public static File one = new File("input");
+    public static File two = new File("output");
 
-    public static void exSort(final File input, final File output) throws IOException {
-        ExternalSort.mergeSortedFiles(ExternalSort.sortInBatch(input), output);
-    }
+    public static void main( String ...args) throws IOException {
+        System.out.println( "Hello Sorter!" );
 
-    public App() {
-        public File one = new File("one");
-        public File two = new File("two");
-
-        public App sortApp = new App();
-        App.exSort(one, two);
+        ExternalSorter es = new ExternalSorter();
+        es.exSort(one, two);
     }
 }
-
---
+++ externalSort/src/main/java/com/sorting/ExternalSorter.java
@@ -0,0 +1,14 @@
+package com.sorting;
+
+import com.google.code.externalsorting.ExternalSort;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExternalSorter {
+
+    public void exSort(final File input, final File output) throws IOException {
+        ExternalSort.mergeSortedFiles(ExternalSort.sortInBatch(input), output);
+    }
+    
+}
